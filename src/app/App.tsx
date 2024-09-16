ac3c4c1404e8bf5c6eb8903aa0d9cf8900043f01
Artem-Andreevich <ops210@mail.ru>
2024-09-16 23:15:39 +0500

    add swiper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,50 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
 import '../scss/styles.scss';
+import swiper from 'swiper';
 
-const listItems = ['first el', 'first el', 'first el']
-// const listItems = ['first el', 'first el', 'first el', 'first el', 'first el', 'first el']
-// const listItems = ['1', '2']
+// const listItems = ['first el', 'first el', 'first el']
+const listItems = ['first el', 'first el', 'first el', 'first el', 'first el', 'first el']
 
 
-export const App = () => {
+export const App = () => { 
+	const swiper = useSwiper();
+		const startPos = 150
+		const offsetAngle = 360 / listItems.length;
+		// const rotateAngle = offsetAngle * i;
 	return (
 		<div className="app">
-			<ul className='circle'>
+			<button onClick={() => swiper.slideNext()}>Slide to the next slide</button>
+			<Swiper
+				slidesPerView='auto'
+				navigation
+				allowTouchMove={false} 
+				loop={true}	
+				// onSlideChangeTransitionStart={
+
+				// }
+			>
+				{listItems.map( (el, i) => {
+					// const startPos = 150
+					// const offsetAngle = 360 / listItems.length;
+					const rotateAngle = offsetAngle * i;
+					return (
+						<SwiperSlide
+							key={i}
+							style={{transform: `rotate(${rotateAngle - startPos}deg) translate(0, calc( (var(--circle-size) - (var(--circle-item-size) / 10) ) / 2)) rotate(${-rotateAngle + startPos}deg)`}}
+						>
+							<li className='list-item' >
+								<span>{el}</span>
+							</li>
+						</SwiperSlide>
+					)
+				})}
+			</Swiper>
+			
+			{/* <ul className='circle'>
 				{listItems.map( (el, i) => {
 					const startPos = 150
 					const offsetAngle = 360 / listItems.length;
@@ -16,14 +52,13 @@ export const App = () => {
 					return (
 						<li 
 							className='list-item' 
-							// style={{transform: `rotate(${rotateAngle - startPos}deg) translate(0, 215px ) rotate(${-rotateAngle + startPos}deg)`}}
 							style={{transform: `rotate(${rotateAngle - startPos}deg) translate(0, calc( (var(--circle-size) - (var(--circle-item-size) / 10) ) / 2)) rotate(${-rotateAngle + startPos}deg)`}}
 						>
 							<span>{el}</span>
 						</li>
 					)
 				})}
-			</ul>
+			</ul> */}
 		</div>
 	);
 }
